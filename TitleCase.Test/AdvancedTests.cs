640a8ee78af3b4105f8152622c7869caa53ade99
danebalia <dane.balia@sourcing-sa.co.za>
2020-09-28 09:22:38 +0200

    Fixed name of advanced test

--- TitleCase.Test/AdvancedTests.cs
+++ TitleCase.Test/AdvancedTests.cs
@@ -13,9 +13,8 @@ namespace TitleCase.Test
         [InlineData("First a of in","an often into","First A Of In")]
         [InlineData("a clash of KINGS","a an the OF","A Clash of Kings")]
         [InlineData("the QUICK bRoWn fOX","xyz fox quick the","The quick Brown fox")]
-        public void BasicNameWithList(string title, string minorWords, string expected)
+        public void AdvancedNameWithList(string title, string minorWords, string expected)
         {
-
             var actual = StringUtilities.TitleCase(title, minorWords);
             Assert.Matches(expected, actual);
         }
